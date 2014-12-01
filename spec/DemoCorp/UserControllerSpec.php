cdff7fffa70e15245351b0d9b8db1c873cf6fe8b
Ciaran McNulty <mail@ciaranmcnulty.com>
2014-12-01 17:54:36 +0000

    Cleaning out UserController to make it clearer

---
+++ spec/DemoCorp/UserControllerSpec.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace spec\DemoCorp;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class UserControllerSpec extends ObjectBehavior
+{
+    function it_saves_users_to_repository_with_name_provided()
+    {
+    }
+
+    function it_looks_up_users_by_id_and_displays_then()
+    {
+    }
+}
--- src/DemoCorp/UserController.php
+++ src/DemoCorp/UserController.php
@@ -6,34 +6,5 @@ use Inviqa\Db;
 
 class UserController
 {
-    private $db;
-
-    public function __construct()
-    {
-        $path = tempnam(sys_get_temp_dir(), 'data-storage');
-
-        if (!is_dir($path)) {
-            unlink($path);
-            mkdir($path);
-        }
-
-        $this->db = new Db($path);
-    }
-
-    public function saveAction($name)
-    {
-        $user = new NamedUser($name);
-        $id = $this->db->saveData(['name' => $user->getName()]);
-
-        return ['id' => $id];
-    }
-
-    public function displayAction($id)
-    {
-        $data = $this->db->loadData($id);
-        $user = new NamedUser($data['username']);
-
-        return ['user' => $user];
-    }
 
 } 
