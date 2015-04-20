e91ffad1320672b5a9674f45192dc7d1eaa1c960
Ciaran McNulty <cmcnulty@inviqa.com>
2015-04-20 11:04:05 +0100

    Comment example stubs

--- spec/DemoCorp/UserControllerSpec.php
+++ spec/DemoCorp/UserControllerSpec.php
@@ -7,11 +7,11 @@ use Prophecy\Argument;
 
 class UserControllerSpec extends ObjectBehavior
 {
-    function it_saves_a_user_with_name_provided_to_the_repository_and_returns_id_as_array()
-    {
-    }
-
-    function it_finds_user_by_id_and_returns_an_array_containing_it()
-    {
-    }
+//    function it_saves_a_user_with_name_provided_to_the_repository_and_returns_id_as_array()
+//    {
+//    }
+//
+//    function it_finds_user_by_id_and_returns_an_array_containing_it()
+//    {
+//    }
 }
